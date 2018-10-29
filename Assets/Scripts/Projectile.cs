0d5bff13e1c1866c01d8aa33b24ff57a37fef37d
Alex Beckwith <abeckwith@beckwith.local>
2018-10-29 07:35:42 -0600

    wip for projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Projectile : MonoBehaviour 
+{
+	public GameObject projectile;
+
+	protected Rigidbody2D rb2d;
+
+	protected Vector2 position;
+	protected Vector3 rotation;
+
+  protected float speed = 7.0f;
+  protected float lifetime = 2.0f;
+  protected float cooldown = 0.66f;
+  protected float lastFireTime = 0.0f; 
+
+	public Projectile(Vector2 _position, Vector3 _rotation)
+	{
+		position = _position;
+		rotation = _rotation;
+	}
+
+	public void Awake()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	public void Fire()
+	{
+    var projectileInstance = Instantiate(
+			projectile, 
+			transform.position, 
+			Quaternion.Euler(rotation)
+		);
+    projectileInstance.GetComponent<Rigidbody2D>().velocity = velocity * speed;
+
+    Destroy(projectileInstance, lifetime);
+	}
+}
